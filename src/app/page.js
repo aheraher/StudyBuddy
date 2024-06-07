ce09f802e2237da8011ae22d5bff365fa699c71e
Dipak Aher <97633930+aheraher@users.noreply.github.com>
2024-06-07 17:49:18 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,24 @@
+import Features from "./componets/Features";
+
+import HeroSection from "./componets/HeroSection";
+import Join from "./componets/Join";
+import Truste from "./componets/Truste";
+import WhatWEOffer from "./componets/WhatWeOffer";
+import LastHeroSec from "./componets/LastHeroSec";
+
+export default function Home() {
+  return (
+   <>
+   
+   
+
+   <HeroSection/>
+   <Features />
+   <Join/>
+   <WhatWEOffer/>
+   <Truste/>
+   <LastHeroSec/>
+
+   </>
+  );
+}
